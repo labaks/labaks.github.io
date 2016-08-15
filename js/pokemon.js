863d03a16484127fd74375bdfaef8d427b3ae551
Alexey <las93@bk.ru>
2016-08-15 14:31:48 +0300

    [pokemon] start table

--- js/pokemon.js
+++ js/pokemon.js
@@ -2,38 +2,40 @@ document.addEventListener("DOMContentLoaded", createTable);
 
 function createTable() {
 // Это количество строк
-var rows = 2; 
+    var rows = 151;
 // Это количество ячеек в строке (количество столбцов)
-var cells = 2;
+    var cells = 2;
 
 // Получить ссылку на body
-var body = document.body;
+    var body = document.body;
 
 // Создать элемент <table> и элемент <tbody>
-var table     = document.createElement("table");
-var tableBody = document.createElement("tbody");
+    var table = document.createElement("table");
+    var tableBody = document.createElement("tbody");
 
 // Создать все ячейки
-for (var j = 0; j < rows; j++) {
-    // Создать строку
-    var row = document.createElement("tr");
+    for (var j = 0; j < rows; j++) {
+        // Создать строку
+        var row = document.createElement("tr");
 
-    for (var i = 0; i < cells; i++) {
-        // Создать и заполнить элемент <td>
-        var cell = document.createElement("td");
-        // Здесь нужно будет заполнить своими данными ячейку
-        var cellText = document.createTextNode("Строка: " + j + ", столбец: " + i);
+        for (var i = 0; i < cells; i++) {
+            // Создать и заполнить элемент <td>
+            var cell = document.createElement("td");
+            if (i === 0) {
+                // Здесь нужно будет заполнить своими данными ячейку
+                var cellText = document.createTextNode(base[j].id);
+            } else var cellText = document.createTextNode(base[j].name);
 
-        cell.appendChild(cellText);
-        row.appendChild(cell);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+
+        // Добавить строку в конец элемента tbody
+        tableBody.appendChild(row);
     }
 
-    // Добавить строку в конец элемента tbody
-    tableBody.appendChild(row);
-}
-
 // Поместить <tbody> внуть <table>
-table.appendChild(tableBody);
+    table.appendChild(tableBody);
 // // Добавить <table> внутрь <body>
-body.appendChild(table);
+    body.appendChild(table);
 }
