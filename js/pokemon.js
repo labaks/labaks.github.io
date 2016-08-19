ec8e6c176743d728ae50f07ee2cf87533faa6a4d
labaks <las93@bk.ru>
2016-08-19 20:16:47 +0300

    [pokemon] try func

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,34 +1,34 @@
 document.addEventListener("DOMContentLoaded", createTable);
 
 function createTable() {
-    var rows = 151;
-    var cells = 4;
+	var rows = 151;
+	var cells = 4;
 
-    var body = document.body;
+	var body = document.body;
 
-    var table = document.createElement("table");
-    var tableBody = document.createElement("tbody");
+	var table = document.createElement("table");
+	var tableBody = document.createElement("tbody");
 
-    for (var j = 0; j < rows; j++) {
-        var row = document.createElement("tr");
+	for (var j = 0; j < rows; j++) {
+		var row = document.createElement("tr");
 
-        for (var i = 0; i < cells; i++) {
-            var cell = document.createElement("td");
-            var cellText;
+		for (var i = 0; i < cells; i++) {
+			var cell = document.createElement("td");
+			var cellText;
             // 0 == id, 1 == name, 2 == nameRus, 3 == maxCp
             switch (i) {
-                case 0:
-                    cellText = document.createTextNode(base[j].id);
-                    break;
-                case 1:
-                    cellText = document.createTextNode(base[j].name);
-                    break;
-                case 2:
-                    cellText = document.createTextNode(base[j].nameRus);
-                    break;
-                case 3:
-                    cellText = document.createTextNode(base[j].maxCp);
-                    break;
+            	case 0:
+            	cellText = document.createTextNode(base[j].id);
+            	break;
+            	case 1:
+            	cellText = document.createTextNode(base[j].name);
+            	break;
+            	case 2:
+            	cellText = document.createTextNode(base[j].nameRus);
+            	break;
+            	case 3:
+            	cellText = document.createTextNode(base[j].maxCp);
+            	break;
             }
             cell.appendChild(cellText);
             row.appendChild(cell);
@@ -39,4 +39,14 @@ function createTable() {
 
     table.appendChild(tableBody);
     body.appendChild(table);
+
+
+    for (i = 0; i < attackDexStandart[0].pokemonsHave.length; i++) {
+    	for (j = 0; j < base.length; j++) {
+    		if (attackDexStandart[0].pokemonsHave[i] === base[j].id) {
+    			console.log(base[j].name);
+    		}
+    	}
+
+    }
 }
