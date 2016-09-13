08db6146b2313631dd49c0483aa9fd88bf8594c6
Alexey <las93@bk.ru>
2016-09-13 16:02:56 +0300

    [multicont] work on cookies

--- multicont/js/chnglng.js
+++ multicont/js/chnglng.js
@@ -1,11 +1,13 @@
-function changeLng(e) {
+function onChangeLng(e) {
+    if ($.cookie('mult_lng') !== e.id) {
+        $.cookie('mult_lng', e.id);
+    }
+    changeLng($.cookie('mult_lng'));
+}
 
-		console.log($.cookie('mult_lng'));
-		console.log(e.id);
-
-	$.cookie('mult_lng', e.id);
-	if ($.cookie('mult_lng')) {
-		console.log("true");
-		console.log($.cookie('mult_lng'));
-	}
+function changeLng(lng) {
+    $('[multilang]').each(function () {
+        for (var i = 0; i < lng.length(); i++)
+        $(this).html();
+    });
 }
