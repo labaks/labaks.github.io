131c7999bdcb652245ed8f7e914a7826b10e5382
Alexey <las93@bk.ru>
2016-09-15 13:49:47 +0300

    [multicont] refactoring chnglng.js

--- multicont/js/chnglng.js
+++ multicont/js/chnglng.js
@@ -7,10 +7,8 @@ var lngBtn = ".languages button";
 
 function checkLng() {
     if ($.cookie(cookiesName)) {
-        changeLng($.cookie(cookiesName));
-        $(lngBtn).each(function () {
-            $(this).removeClass(activeBtnClass);
-        });
+        replacementWords($.cookie(cookiesName));
+        $(lngBtn).removeClass(activeBtnClass);
         $("#" + $.cookie(cookiesName)).addClass(activeBtnClass);
         console.log($.cookie(cookiesName));
     }
@@ -20,80 +18,62 @@ function onChangeLng(e) {
     if ($.cookie(cookiesName) !== e.id) {
         $.cookie(cookiesName, e.id);
     }
-    changeLng($.cookie(cookiesName));
-//    changeLng(e.id);
-    $(lngBtn).each(function () {
-        $(this).removeClass(activeBtnClass);
-    });
+    replacementWords($.cookie(cookiesName));
+//    replacementWords(e.id);
+    $(lngBtn).removeClass(activeBtnClass);
     $(e).addClass(activeBtnClass);
 }
 
-function changeLng(lng) {
-    switch (lng) {
-        case 'ru':
-            replacementWords(ru);
-            break;
-        case 'en':
-            replacementWords(en);
-            break;
-        case 'ua':
-            replacementWords(ua);
-            break;
-        default :
-            break;
-    }
-}
-
-function replacementWords(obj) {
+function replacementWords(lang) {
     $('[' + attrName + ']').each(function () {
-        $(this).html(obj[$(this).attr(attrName)]);
+        $(this).html(langs[lang][$(this).attr(attrName)]);
     });
 }
 
-var ru = {
-    "main": "Главная",
-    "phone": "Телефон:",
-    "fax": "Факс:",
-    "about_us": "О нас",
-    "services": "Услуги",
-    "forwarding_services": "Экспедиторские услуги",
-    "customs_brokerage_services": "Таможенно-брокерские услуги",
-    "logistics": "Логистика",
-    "commerce_brokerage_services": "Торгово-брокерские услуги (ВЭД)",
-    "professional_translation_of_documents": "Профессиональный перевод документов",
-    "affreightment": "Фрахтование",
-    "contacts": "Контакты",
-    "copyright": "&copy; Логистическая компания 'Multicont, Одесса'"
-};
-
-var en = {
-    "main": "Main",
-    "phone": "Phone:",
-    "fax": "Fax:",
-    "about_us": "About us",
-    "services": "Services",
-    "forwarding_services": "Forwarding services",
-    "customs_brokerage_services": "Customs-brokerage services",
-    "logistics": "Logistics",
-    "commerce_brokerage_services": "Commerce and brokerage services",
-    "professional_translation_of_documents": "Proffessional translation of documents",
-    "affreightment": "Affreightment",
-    "contacts": "Contacts",
-    "copyright": "&copy; Logistics company 'Multicont, Odessa'"
-};
-
-var ua = {
-    "main": "Головна",
-    "phone": "Телефон:",
-    "fax": "Факс:",
-    "about_us": "Про нас",
-    "services": "Послуги",
-    "forwarding_services": "Експедиторські послуги",
-    "customs_brokerage_services": "Митно-брокерські послуги",
-    "logistics": "Логістика",
-    "commerce_brokerage_services": "Торгово-брокерські послуги (ЗЕД)",
-    "professional_translation_of_documents": "Професійний переклад документів",
-    "affreightment": "Фрахтування",
-    "contacts": "Контакти",
-    "copyright": "&copy; Логістична компанія 'Multicont, Одеса'"
+var langs = {
+    "ru": {
+        "main": "Главная",
+        "phone": "Телефон:",
+        "fax": "Факс:",
+        "about_us": "О нас",
+        "services": "Услуги",
+        "forwarding_services": "Экспедиторские услуги",
+        "customs_brokerage_services": "Таможенно-брокерские услуги",
+        "logistics": "Логистика",
+        "commerce_brokerage_services": "Торгово-брокерские услуги (ВЭД)",
+        "professional_translation_of_documents": "Профессиональный перевод документов",
+        "affreightment": "Фрахтование",
+        "contacts": "Контакты",
+        "copyright": "&copy; Логистическая компания 'Multicont, Одесса'"
+    },
+    "en": {
+        "main": "Main",
+        "phone": "Phone:",
+        "fax": "Fax:",
+        "about_us": "About us",
+        "services": "Services",
+        "forwarding_services": "Forwarding services",
+        "customs_brokerage_services": "Customs-brokerage services",
+        "logistics": "Logistics",
+        "commerce_brokerage_services": "Commerce and brokerage services",
+        "professional_translation_of_documents": "Proffessional translation of documents",
+        "affreightment": "Affreightment",
+        "contacts": "Contacts",
+        "copyright": "&copy; Logistics company 'Multicont, Odessa'"
+    },
+    "ua": {
+        "main": "Головна",
+        "phone": "Телефон:",
+        "fax": "Факс:",
+        "about_us": "Про нас",
+        "services": "Послуги",
+        "forwarding_services": "Експедиторські послуги",
+        "customs_brokerage_services": "Митно-брокерські послуги",
+        "logistics": "Логістика",
+        "commerce_brokerage_services": "Торгово-брокерські послуги (ЗЕД)",
+        "professional_translation_of_documents": "Професійний переклад документів",
+        "affreightment": "Фрахтування",
+        "contacts": "Контакти",
+        "copyright": "&copy; Логістична компанія 'Multicont, Одеса'"
+    }
 };
